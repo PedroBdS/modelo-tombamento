b75951504b04e30dccba12e19ed3f63d55f82f5b
Silva <PBOTTARI@ball.com>
2024-09-12 09:35:14 -0300

    vs

--- app.py
+++ app.py
@@ -9,17 +9,17 @@ import plotly.express as px
 
 @st.cache_resourse
 def carrega_modelo():
-    uploaded_file = st.file_uploader('Arraste e solte o modelo aqui', type=['h5'])
+    # uploaded_file = st.file_uploader('Arraste e solte o modelo aqui', type=['h5'])
 
-    if uploaded_file is not None:
-        return uploaded_file
+    # if uploaded_file is not None:
+    #     return uploaded_file
 
-    # # https://drive.google.com/file/d/1QWGjUvJ6XNdRnLpL-2tuT6DMyoQcyLbe/view?usp=drive_link/view?usp=drive_link
-    # url = 'https://drive.google.com/uc?id=1QWGjUvJ6XNdRnLpL-2tuT6DMyoQcyLbelsCrarV0182Jkh'
-    # gdown.download(url, 'modelo_quantizado16bits.tflite')
-    # interpreter = tf.lite.Interpreter(model_path='modelo_quantizado16bits.tflite')
-    # interpreter.allocate_tensors()
-    # return interpreter
+    # https://drive.google.com/file/d/1QWGjUvJ6XNdRnLpL-2tuT6DMyoQcyLbe/view?usp=drive_link/view?usp=drive_link
+    url = 'https://drive.google.com/uc?id=1QWGjUvJ6XNdRnLpL-2tuT6DMyoQcyLbelsCrarV0182Jkh'
+    gdown.download(url, 'modelo_quantizado16bits.tflite')
+    interpreter = tf.lite.Interpreter(model_path='modelo_quantizado16bits.tflite')
+    interpreter.allocate_tensors()
+    return interpreter
 
 def carrega_imagem():
     uploaded_file = st.file_uploader('Arraste e solte uma imagem aqui ou clique para selecionar uma', type=['png', 'jpg', 'jpeg'])
@@ -42,33 +42,35 @@ def previsao(interpreter,image):
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
     
-    interpreter.set_tensor(input_details[0]['index'],imagem) 
+    interpreter.set_tensor(input_details[0]['index'],image) 
     
     interpreter.invoke()
     
     output_data = interpreter.get_tensor(output_details[0]['index'])
-    classes = ['BlackMeasles', 'BlackRot', 'HealthyGrapes', 'LeafBlight']
+    classes = ['tombada']
 
     df = pd.DataFrame()
     df['classes'] = classes
     df['probabilidades (%)'] = 100*output_data[0]
     
-    fig = px.bar(df,y='classes',x='probabilidades (%)',  orientation='h', text='probabilidades (%)', title='Probabilidade de Classes de Doenças em Uvas')
+    fig = px.bar(df,y='classes',x='probabilidades (%)',  orientation='h', text='probabilidades (%)', title='Probabilidade lata estar tombada')
 
 
 def main():
+
     st.set_page_config(
-        page_title="Classifica Folhas de Videira",
+        page_title="Identifica latas tombadas",
         page_icon="Ball",
     )
 
     st.write("# Identifica latas tombadas")
     #Carrega modelo
+    interpreter = carrega_modelo()
     #Carrega imagem
     image = carrega_imagem()
     #Classifica
     if image is not None:
-        previsao(interpreter,image)
+        previsao(interpreter, image)
             
 
 if __name__ == "__main__":
